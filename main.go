ff7e2f9cb48dc329f5662d655be00585640599af
T-death <suketawada0612@gmail.com>
2024-10-20 16:21:32 +0900

    [add] 第4章終了

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+func f(ctx context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+			case <-ctx.Done():
+				return
+			default:
+		}
+		fmt.Println("gorutine: 処理")
+		time.Sleep(1 * time.Second)
+	}
+}
 
 func main() {
-	fmt.Println("Hello World")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	ctx, cancel := context.WithCancel(context.Background())
+	go f(ctx, &wg)
+	
+	time.Sleep(10 * time.Second)
+	cancel()
+	wg.Wait()
 }
